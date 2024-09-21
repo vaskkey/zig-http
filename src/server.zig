492267e4bf845b9eae161bd81aa43f5990ac376a
Awraam <awraam.waskowski@gmail.com>
2024-09-21 17:07:27 +0200

    feat: Add response object

--- src/server.zig
+++ src/server.zig
@@ -5,6 +5,14 @@ const net = std.net;
 pub const Header = struct {
     key: []const u8,
     value: []const u8,
+
+    pub fn get_str(self: *Header, allocator: std.mem.Allocator) ![]u8 {
+        return try std.fmt.allocPrint(allocator, "{s}: {s}", .{ self.key, self.value });
+    }
+
+    pub fn len(self: *const Header) usize {
+        return self.key.len + self.value.len + 2;
+    }
 };
 
 /// HTTP method
@@ -35,6 +43,48 @@ pub const Request = struct {
         }
 
         return null;
+    }
+};
+
+/// HTTP request from client
+pub const Response = struct {
+    /// Response headers
+    headers: []*Header,
+    /// Response body
+    body: []const u8,
+    /// Response status
+    status: u8,
+
+    pub fn to_str(self: *Response, allocator: std.mem.Allocator) ![]u8 {
+        const headers = try self.get_headers_str(allocator);
+        return try std.fmt.allocPrint(allocator, "HTTP/1.1 {d} {s}\r\n{s}\r\n{s}", .{ self.status, "Created", headers, self.body }); // TODO: Add a map from status to status string
+    }
+
+    fn get_headers_str(self: *Response, allocator: std.mem.Allocator) ![]u8 {
+        if (self.headers.len == 0) return "";
+        const sep = "\r\n";
+
+        const total_len = blk: {
+            var sum: usize = sep.len * (self.headers.len);
+            for (self.headers) |header| sum += header.len();
+            break :blk sum;
+        };
+
+        const buf = try allocator.alloc(u8, total_len);
+        errdefer allocator.free(buf);
+
+        @memcpy(buf[0..self.headers[0].len()], try self.headers[0].get_str(allocator));
+        var buf_index: usize = self.headers[0].len();
+        @memcpy(buf[buf_index .. buf_index + sep.len], sep);
+        buf_index += sep.len;
+        for (self.headers[1..]) |header| {
+            @memcpy(buf[buf_index .. buf_index + header.len()], try header.get_str(allocator));
+            buf_index += header.len();
+            @memcpy(buf[buf_index .. buf_index + sep.len], sep);
+            buf_index += sep.len;
+        }
+
+        return buf;
     }
 };
 
@@ -85,7 +135,16 @@ pub fn GetServer() type {
             const req = try self.parse(&buffer, bytesRead);
             defer self.allocator.destroy(req);
 
-            _ = try writer.write("HTTP/1.1 200 OK\r\n\r\n");
+            const response = try self.allocator.create(Response);
+            defer self.allocator.destroy(response);
+
+            response.headers = &[0]*Header{};
+            response.body = "HHHH";
+            response.status = 201;
+
+            const str = try response.to_str(self.allocator);
+
+            _ = try writer.write(str);
         }
 
         fn parse(self: *Self, buffer: []u8, bytes_read: usize) !*Request {
